197bbf738f89adc563b33a1916883a0ef78fb6c3
itooww <12472231+itooww@users.noreply.github.com>
2019-08-17 15:32:55 +0900

    Add: delete_not_exist_lambda_log_groups, etc

--- cleaner/app.py
+++ cleaner/app.py
@@ -12,6 +12,8 @@ logger.setLevel(INFO)
 logs_client = boto3.client('logs')
 lambda_client = boto3.client('lambda')
 
+LAMBDA_FUNCTION_LOG_GROUP_NAME_PREFIX = '/aws/lambda/'
+
 def get_log_group_names():
     """
     CloudWatch Logs の全てのロググループ情報を取得し、ロググループ名のリストを返す
@@ -84,17 +86,71 @@ def get_lambda_function_names():
 
     return lambda_function_names
 
+def extract_lambda_log_group_names(log_group_names):
+    """
+    ロググループ名のリストを受け取り、そこから Lambda 関数ロググループ名を抽出したリストを返す
+
+    Parameters
+    ----------
+    log_group_names: list, required
+        ロググループ名のリスト
+
+    Returns
+    -------
+    lambda_function_names: list
+        Lambda 関数ロググループ名のリスト
+    """
+    lambda_log_group_names = list()
+
+    for log_group_name in log_group_names:
+        if LAMBDA_FUNCTION_LOG_GROUP_NAME_PREFIX in log_group_name:
+            lambda_log_group_names.append(log_group_name)
+
+    return lambda_log_group_names
+
+def delete_not_exist_lambda_log_groups(lambda_log_group_names, lambda_function_names):
+    """
+    Lambda 関数の全ての情報を取得し、Lambda 関数名のリストを返す
+
+    Parameters
+    ----------
+    lambda_log_group_names: list, required
+        Lambda 関数ロググループ名のリスト
+
+    lambda_function_names: list, required
+        Lambda 関数名のリスト
+    """
+
+    lambda_function_names_concatenate_log_group_prefix = list()
+
+    for lambda_function_name in lambda_function_names:
+        lambda_function_names_concatenate_log_group_prefix.append(
+            LAMBDA_FUNCTION_LOG_GROUP_NAME_PREFIX + lambda_function_name
+        )
+
+    not_exist_lambda_log_groups = list(set(lambda_log_group_names) - set(lambda_function_names_concatenate_log_group_prefix))
+    logger.info('delete lambda log group list')
+    logger.info(json.dumps(not_exist_lambda_log_groups, ensure_ascii=False, indent=2))
+
+    # for not_exist_lambda_log_group in not_exist_lambda_log_groups:
+    #     logs_client.delete_log_group(logGroupName=not_exist_lambda_log_group)
+
 def lambda_handler(event, context):
 
     # recieved event
     logger.info(json.dumps(event, ensure_ascii=False, indent=2))
 
-    # get log groups list
+    # get all log groups list
     log_group_names = get_log_group_names()
     logger.debug(json.dumps(log_group_names, ensure_ascii=False, indent=2))
 
+    # get lambda function names
     lambda_function_names = get_lambda_function_names()
     logger.debug(json.dumps(lambda_function_names, ensure_ascii=False, indent=2))
+
+    # delete not exit lambda log groups
+    lambda_log_group_names = extract_lambda_log_group_names(log_group_names)
+    delete_not_exist_lambda_log_groups(lambda_log_group_names, lambda_function_names)
 
     return {
         "statusCode": 200,
